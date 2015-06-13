ae243057726dc6d7e48f3078a848dba1f61f45a6
Ramy Kfoury <kfoury.r@gmail.com>
2015-06-13 11:10:12 +0200

    retry image download after delay, until cached image is retrieved

---
+++ Library/MapleBacon/MapleBacon/Delay.swift
@@ -0,0 +1,15 @@
+//
+//  Copyright (c) 2015 Zalando SE. All rights reserved.
+//
+
+import Foundation
+
+
+func delay(delay:Double, closure:()->()) {
+    dispatch_after(
+        dispatch_time(
+            DISPATCH_TIME_NOW,
+            Int64(delay * Double(NSEC_PER_SEC))
+        ),
+        dispatch_get_main_queue(), closure)
+}

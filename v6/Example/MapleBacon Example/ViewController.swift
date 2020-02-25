a0cd5bd61437d9dae05f7ac02549f54a0a49d866
Jan Gorman <gorman.jan@gmail.com>
2020-02-25 18:50:31 +0100

    Add new example app

---
+++ v6/Example/MapleBacon Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapleBacon Example
+//
+//  Created by Jan Gorman on 25.02.20.
+//  Copyright © 2020 Schnaub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+

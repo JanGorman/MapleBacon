0469c6ebf6078aaf71d64c2ef859e2966e2e402a
Jan Gorman <gorman.jan@gmail.com>
2020-03-01 12:06:53 +0100

    Image Transformer Example

---
+++ v6/Example/MapleBacon Example/Controllers/ImageTransformerViewController.swift
@@ -0,0 +1,81 @@
+//
+//  Copyright © 2020 Schnaub. All rights reserved.
+//
+
+import MapleBacon
+import UIKit
+
+final class ImageTransformerViewController: UICollectionViewController {
+
+  private var imageURLs: [URL] = []
+  private var imageTransformer = SepiaImageTransformer() >>> VignetteImageTransformer()
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    imageURLs = imageURLsFromBundle()
+    collectionView?.reloadData()
+  }
+
+  private func imageURLsFromBundle() -> [URL] {
+    let file = Bundle.main.path(forResource: "images", ofType: "plist")!
+    let urls = NSArray(contentsOfFile: file) as! [String]
+    return urls.compactMap { URL(string: $0) }
+  }
+
+  override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    imageURLs.count
+  }
+
+  override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell: ImageCollectionViewCell = collectionView.dequeue(indexPath: indexPath)
+    let url = imageURLs[indexPath.item]
+    cell.imageView.setImage(with: url, displayOptions: [.downsampled], imageTransformer: imageTransformer)
+    return cell
+  }
+
+}
+
+private class SepiaImageTransformer: ImageTransforming {
+
+  let identifier = "com.schnaub.SepiaImageTransformer"
+
+  func transform(image: UIImage) -> UIImage? {
+    let filter = CIFilter(name: "CISepiaTone")!
+
+    let ciImage = CIImage(image: image)
+    filter.setValue(ciImage, forKey: kCIInputImageKey)
+    filter.setValue(0.5, forKey: kCIInputIntensityKey)
+
+    let context = CIContext()
+    guard let outputImage = filter.outputImage,
+          let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {
+            return image
+    }
+
+    return UIImage(cgImage: cgImage)
+  }
+
+}
+
+private class VignetteImageTransformer: ImageTransforming {
+
+  let identifier = "com.schnaub.VignetteImageTransformer"
+
+  func transform(image: UIImage) -> UIImage? {
+    let filter = CIFilter(name: "CIVignette")!
+
+    let ciImage = CIImage(image: image)
+    filter.setValue(ciImage, forKey: kCIInputImageKey)
+
+    let context = CIContext()
+    guard let outputImage = filter.outputImage,
+          let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {
+            return image
+    }
+
+    return UIImage(cgImage: cgImage)
+  }
+
+}
+
